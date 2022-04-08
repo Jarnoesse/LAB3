4158b0d8b780233274aa50e5d6975dda77789a9d
Jarno Sandrin <jarnosandrin@gmail.com>
2022-04-08 14:33:10 +0200

    testino
    stsdzgx

--- roottest/main.py
+++ roottest/main.py
@@ -1,4 +1,4 @@
 # qui runno tutto, che scasso
-#exec(open("dcr_oct_temp.py").read())
-#exec(open("absorption.py").read())
+exec(open("dcr_oct_temp.py").read())
+exec(open("absorption.py").read())
 exec(open("dcr_oct.py").read())
