8e17464e74d6a69fc0b48d35e3ba28c9ba933bcb
Jarno Sandrin <jarnosandrin@gmail.com>
2022-04-25 10:53:30 +0200

    susamogus
    sus

--- roottest/absorption.py
+++ roottest/absorption.py
@@ -12,16 +12,17 @@ print("---------------------")
 
 #dato che c'è da copiere la stessa operazione per carta e metallo definisco una funzione.
 def Elaborazione(filepath,total_width,total_error,nsheets):
-
+    
+    #gatewidth
     tau = 0.8
     #faccio la lista di file di dati da utilizzare.
     filelist=glob.glob(filepath)
     
     mu_df = pd.DataFrame(columns=["fogli","pvalue","media","mediaerr","varianza","varianzaerr"])
-    bridge_df = pd.DataFrame(columns=["L","sigmaL","N","sigmaN"])
+    bridge_df = pd.DataFrame(columns=["N","sigmaN","L","sigmaL"])
     
     width = total_width/nsheets      #mm
-    width_error = total_error/20 #mm
+    width_error = total_error        #mm
     
     #utilizzo un loop per navigare fra i fogli di dati
     for filename in filelist:
@@ -43,7 +44,9 @@ def Elaborazione(filepath,total_width,total_error,nsheets):
         mu_df.loc[fogli] = [fogli,df.loc[0,0],df.loc[0,1],df.loc[0,2],df.loc[0,3],df.loc[0,4]]
     
         #fillo il dataframe per il secondo fit.
-        bridge_df.loc[fogli] = [float(fogli)*width,width_error,df.loc[0,1]/tau,np.sqrt(df.loc[0,1])/tau]
+        #  L sL N sN
+        bridge_df.loc[fogli] = [df.loc[0,1]/tau,np.sqrt(df.loc[0,1])/tau,float(fogli)*width,width_error,]
+
         
     #ora faccio i preparativi per il fit con l'esponenziale negativa.
     #salvo il secondo dataframe, che è quello che contiene i dati da fittare!
@@ -58,22 +61,23 @@ def Elaborazione(filepath,total_width,total_error,nsheets):
     return bridge_df["N"],bridge_df["L"],bridge_df["sigmaN"],bridge_df["sigmaL"],pars_df.loc[0,1],pars_df.loc[0,3]
     
 #CARTA
-N,L,sN,sL,p0,p1 = Elaborazione("./Data/Assorbimento/carta/*.txt",2,1.5,22)
+N,L,sN,sL,p0,p1 = Elaborazione("./Data/Assorbimento/carta/*.txt",20+1.5/20,1/20,22)
+
 
 # il grafico
-x = np.linspace(700,1800,1000)
+x = np.linspace(0,20,800)
 y = p0*np.exp(-p1*x)
 
-plt.errorbar(N,L,yerr=sL,xerr=sN,fmt='o')
+plt.errorbar(L,N,yerr=sN,xerr=sL,fmt='o')
 plt.plot(x,y,'b')
 
 #ALUMINIO
-N2,L2,sN2,sL2,p02,p12 = Elaborazione("./Data/Assorbimento/metallo/*.txt",3,1.5,20)
+N2,L2,sN2,sL2,p02,p12 = Elaborazione("./Data/Assorbimento/metallo/*.txt",3/20,1/20,20)
 
-x2 = np.linspace(1200,1800,1000)
+x2 = np.linspace(0,3/20,800)
 y2 = p02*np.exp(-p12*x2)
 
-plt.errorbar(N2,L2,yerr=sL2,xerr=sN2,fmt='o')
+plt.errorbar(L2,N2,yerr=sN2,xerr=sL2,fmt='o')
 plt.plot(x2,y2,'r')
 plt.show()
 
